da3bbbcbbe0f5259460adb15eb9b8de2cab6a2b0
Michael-August <michaelfrancis135@gmail.com>
2023-06-22 11:54:33 +0100

    Landing page done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ChatPageComponent } from './pages/chat-page/chat-page.component';
 import { PremiumAdPageComponent } from './pages/premium-ad-page/premium-ad-page.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 
 const routes: Routes = [
   {
     path: 'chat', component: ChatPageComponent
+  },
+  {
+    path: '', component: LandingPageComponent
   },
   {
     path: 'sub-detail', component: PremiumAdPageComponent
--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
@@ -7,9 +9,85 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
+
+  paymentDurationOption = {
+    status: 'active',
+    durationOption: 'monthly',
+    price: '#500'
+  }
+
+  isLoading: boolean = false
+
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(8)])
+  })
+
+  signinForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    phone: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required, Validators.minLength(8)])
+  })
+
+  openAuthModal: boolean = false
+  openLogInModal: boolean = false
+  openSignUpModal: boolean = false
+
+  user: any
 
   ngOnInit(): void {
+  }
+
+  switchDurationOption(duration: string) {
+    switch (duration) {
+      case 'monthly':
+        this.paymentDurationOption.price = '#500'
+        this.paymentDurationOption.durationOption = 'monthly'
+        break;
+      case 'annual':
+        this.paymentDurationOption.price = '#5,000'
+        this.paymentDurationOption.durationOption = 'annual'
+        break
+      default:
+        break;
+    }
+  }
+
+  closeModalAction () {
+    this.openAuthModal = false
+  }
+
+  signup() {
+
+  }
+
+  login() {
+
+  }
+
+  openForm(form: string) {
+    this.openAuthModal = true
+    switch (form) {
+      case 'login':
+        this.openLogInModal = true
+        this.openSignUpModal = false
+        break;
+      case 'signup':
+        this.openSignUpModal = true
+        this.openLogInModal = false
+        break;
+      default:
+        break;
+    }
+  }
+
+  goToSub() {
+
+  }
+
+  goBack() {
+    this.router.navigateByUrl('chat')
   }
 
 }
